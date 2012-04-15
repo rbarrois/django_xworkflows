08de050658dfbaac9ae800cacea6f93deaf438ac
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2012-04-15 17:52:15 +0200

    Don't expose external objects in __init__
    Signed-off-by: Raphaël Barrois <raphael.barrois@polyconseil.fr>

--- src/django_xworkflows/__init__.py
+++
@@ -1,7 +0,0 @@
-import xworkflows
-
-AbortTransition = xworkflows.AbortTransition
-InvalidTransitionError = xworkflows.InvalidTransitionError
-transition = xworkflows.transition
-Workflow = xworkflows.Workflow
-
