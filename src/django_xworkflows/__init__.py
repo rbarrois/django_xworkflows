9bb2c0ecf22a3b4c1c8b3ddffa59d642bc8aad83
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-08-17 12:34:15 +0200

    Version bump.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- src/django_xworkflows/__init__.py
+++ src/django_xworkflows/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2011-2012 Raphaël Barrois
 
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 __author__ = 'Raphaël Barrois <raphael.barrois@polytechnique.org>'
