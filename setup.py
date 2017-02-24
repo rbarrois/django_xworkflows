a5fde85b9e482bee704b62486c9f61142540605d
Damien Baty <damien.baty@polyconseil.fr>
2017-02-24 12:24:49 +0100

    django_xworkflows supports Python 3.6.

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests.runner.runtests',
     zip_safe=False,  # Prevent distribution as eggs for South.
