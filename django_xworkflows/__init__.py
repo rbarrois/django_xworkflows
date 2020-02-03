0ba8fde55c09323a20d92abbe01c327b6857b74e
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2020-02-03 11:38:34 +0100

    Back to development: 0.13.1.dev0+peopledoc

--- django_xworkflows/__init__.py
+++ django_xworkflows/__init__.py
@@ -2,5 +2,5 @@
 # Copyright (c) 2011-2013 Raphaël Barrois
 # This code is distributed under the two-clause BSD license.
 
-__version__ = '0.13.0+peopledoc'
+__version__ = '0.13.1.dev0+peopledoc'
 __author__ = 'Raphaël Barrois <raphael.barrois+xworkflows@polytechnique.org>'
