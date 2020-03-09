74541ca3aa53409ba448e94e0802a11917784026
Raphaël Barrois <raphael.barrois@polytechnique.org>
2020-03-09 19:14:14 +0100

    Fix travis-ci test running.
    Include migrations for the `djworkflows` project.

--- django_xworkflows/xworkflow_log/migrations/0001_initial.py
+++ django_xworkflows/xworkflow_log/migrations/0001_initial.py
@@ -11,7 +11,7 @@ XWORKFLOWS_USER_MODEL = getattr(settings, 'XWORKFLOWS_USER_MODEL', settings.AUTH
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0001_initial'),
+        ('contenttypes', '0002_remove_content_type_name'),
         migrations.swappable_dependency(XWORKFLOWS_USER_MODEL),
     ]
 
