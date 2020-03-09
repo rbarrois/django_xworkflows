508825b8daf7d38c3be5d17893549ab0456d4ae5
Raphaël Barrois <raphael.barrois@polytechnique.org>
2020-03-09 19:47:52 +0100

    Preparing release 1.0.0

--- django_xworkflows/__init__.py
+++ django_xworkflows/__init__.py
@@ -2,5 +2,5 @@
 # Copyright (c) 2011-2020 Raphaël Barrois
 # This code is distributed under the two-clause BSD license.
 
-__version__ = '0.12.4.dev0'
+__version__ = '1.0.0'
 __author__ = 'Raphaël Barrois <raphael.barrois+xworkflows@polytechnique.org>'
